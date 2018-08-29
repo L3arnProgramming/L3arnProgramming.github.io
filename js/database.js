1f803ba7714d9dfa2fab7b7621c6e65574810eea
L3arnProgramming <42762178+L3arnProgramming@users.noreply.github.com>
2018-08-29 08:00:00 +0300

    Add files via upload

---
+++ js/database.js
@@ -0,0 +1,32 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAsvT8EtYUYKLHq00W8v8Gv5a_qwB6Q_J4",
+    authDomain: "l3arnprogramming.firebaseapp.com",
+    databaseURL: "https://l3arnprogramming.firebaseio.com",
+    projectId: "l3arnprogramming",
+    storageBucket: "l3arnprogramming.appspot.com",
+    messagingSenderId: "443020797481"
+};
+firebase.initializeApp(config);
+var db = firebase.firestore();
+
+function readDB(arg)
+{
+    db.collection(arg).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            document.getElementById("title").innerHTML = doc.id;//doc.data()["title"];
+            document.getElementById("text").innerHTML = doc.data()["css styles"];
+            console.log(doc.data(), doc.id);
+        });
+    });
+}
+function writeDB(arg, dataArray)
+{
+    db.collection(arg).add(dataArray).then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    }).catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+}
+//readDB("html");
